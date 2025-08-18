b806d6d25464c21bc4f91a51eea4d3f44d69ab5f
Nguyen Hoang Khoi <116591477+HoangKhoiNguyenLHP@users.noreply.github.com>
2025-08-18 10:08:55 +0700

    Init project

---
+++ app/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello world</Text>
+      <Link href="/signup">Signup</Link>
+      <Link href="/login">Login</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "blue"
+  }
+});
