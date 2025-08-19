cfc07388332c6c67f4d6f3fc7c36cd26afdf08a6
NHKhoi <nhaphk@gmail.com>
2025-08-20 00:59:09 +0700

    Add env variables

--- constants/api.js
+++ constants/api.js
@@ -5,6 +5,7 @@ let API_URL = `${process.env.EXPO_PUBLIC_API_URL}` || "";
 if (__DEV__) {
   // development
   // API_URL = "http://vscodeConsole:4000/api"; // use your WiFi IP if testing on a real phone
+  API_URL = `http://${process.env.EXPO_PUBLIC_WIFI_ADDRESS}:4000/api`;
 
   if(Platform.OS === "android") {
     API_URL = "http://10.0.2.2:4000/api"; // Android Emulator
