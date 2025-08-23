1fe35c57a6a84012cfcd3468ec180a614affde2f
NHKhoi <nhaphk@gmail.com>
2025-08-23 20:49:29 +0700

    Build edit book UI

--- app/admin/book-edit/[id].jsx
+++ app/admin/book-edit/[id].jsx
@@ -1,12 +1,295 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Platform,
+  KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter, useSegments } from "expo-router";
+import styles from "../../../assets/styles/create.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../../constants/colors";
+import { useAuthStore } from "../../../store/authStore";
+
+import * as ImagePicker from "expo-image-picker";
+import * as DocumentPicker from "expo-document-picker";
+import { API_URL } from "../../../constants/api";
 import { useLocalSearchParams, } from "expo-router";
 
 export default function BookDetailPage() {
   const { id } = useLocalSearchParams();
 
+  const segments = useSegments();
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [loading, setLoading] = useState(false);
+
+  const [image, setImage] = useState(null); // display the selected image
+  const [file, setFile] = useState(null);
+
+  const router = useRouter();
+  const { token, checkAuth } = useAuthStore();
+
+  const renderRatingPicker = () => {
+    const stars = [];
+    for(let i = 1; i <= 5; i++) {
+      stars.push(
+        <TouchableOpacity 
+          key={i} 
+          onPress={() => setRating(i)} 
+          style={styles.starButton}
+        >
+          <Ionicons
+            name={i <= rating ? "star" : "star-outline"}
+            size={32}
+            color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+          />
+        </TouchableOpacity>
+      );
+    }
+    return (
+      <View style={styles.ratingContainer}>
+        {stars}
+      </View>
+    );
+  };
+
+  const chooseImage = async () => {
+    try {
+      // request permission if needed
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if(status !== "granted") {
+        Alert.alert(
+          "Permission Denied", 
+          "We need camera roll permissions to upload an image");
+        return;
+      }
+
+      // open image library
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images"],
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 0.5, // lower quality
+      });
+
+      if(!result.canceled) {
+        setImage(result.assets[0].uri);
+      }
+    } 
+    catch (error) {
+      console.error("Error picking image:", error);
+      Alert.alert("Error", "There was a problem selecting your image");
+    }
+  };
+
+  const chooseFile = async () => {
+    try {
+      // open file explorer
+      const result = await DocumentPicker.getDocumentAsync({
+        type: "application/pdf",
+        copyToCacheDirectory: true,
+      });
+
+      if(result.canceled) {
+        return;
+      }
+
+      const fileAsset = result.assets[0];
+      setFile(fileAsset);
+    } 
+    catch (error) {
+      console.error("Error picking PDF:", error);
+      Alert.alert("Error", "Could not pick file!");
+    }
+  };
+
+  const handleSubmit = async () => {
+    if(!title || !caption || !image || !rating || !file) {
+      Alert.alert("Error", "Please fill in all fields");
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const uriParts = image.split(".");
+      const fileType = uriParts[uriParts.length - 1];
+      const imageType = fileType ? `image/${fileType.toLowerCase()}` : "image/jpeg";
+
+      // FormData
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("caption", caption);
+      formData.append("rating", rating.toString());
+
+      formData.append("image", {
+        uri: image, // file path from expo-image-picker
+        name: `upload.${fileType}`, // any name
+        type: imageType, // mime type
+      });
+
+      formData.append("fileBook", {
+        uri: file.uri, // just different style of writing code, actually 100% the same
+        name: file.name,
+        type: "application/pdf",
+      });
+
+      const response = await fetch(`${API_URL}/books`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        body: formData,
+      });
+      const data = await response.json();
+
+      if(!response.ok) { 
+        throw new Error(data.message || "Something went wrong");
+      }
+
+      Alert.alert("Success", "Your book has been posted!");
+      setTitle("");
+      setCaption("");
+      setRating(3);
+      setImage(null);
+      setFile(null);
+      router.push("/admin/(tabs)/");
+    } 
+    catch (error) {
+      console.error("Error creating post:", error);
+      Alert.alert("Error", error.message || "Something went wrong");
+    } 
+    finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, [segments]); // used for simulating in remove token
+
   return (
-    <View>
-      <Text>Edit Screen: {id}</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView contentContainerStyle={styles.container} style={styles.scrollViewStyle}>
+        <View style={styles.card}>
+          <View style={styles.header}>
+            <Text>Book Edit detail: {id}</Text>
+            <Text style={styles.title}>Edit Book</Text>
+            <Text style={styles.subtitle}>Update your books</Text>
+          </View>
+
+          <View style={styles.form}>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Title</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="book-outline"
+                  size={20}
+                  color={COLORS.textSecondary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter book title"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={title}
+                  onChangeText={setTitle}
+                />
+              </View>
+            </View>
+
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Your Rating</Text>
+              {renderRatingPicker()}
+            </View>
+
+            {/* image */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Image</Text>
+              <TouchableOpacity 
+                style={styles.imagePicker} 
+                onPress={chooseImage}
+              >
+                {image ? (
+                  <Image source={{ uri: image }} style={styles.previewImage} />
+                ) : (
+                  <View style={styles.placeholderContainer}>
+                    <Ionicons name="image-outline" size={40} color={COLORS.textSecondary} />
+                    <Text style={styles.placeholderText}>Tap to select image</Text>
+                  </View>
+                )}
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Caption</Text>
+              <TextInput
+                style={styles.textArea}
+                placeholder="Write your thoughts..."
+                placeholderTextColor={COLORS.placeholderText}
+                value={caption}
+                onChangeText={setCaption}
+                multiline
+              />
+            </View>
+
+            {/* pdf */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>PDF file</Text>
+              <TouchableOpacity 
+                style={styles.filePicker} 
+                onPress={chooseFile}
+              >
+                {file ? (
+                  <View style={styles.fileInfo}>
+                    <Ionicons name="document-text-outline" size={28} color={COLORS.primary} />
+                    <Text style={styles.fileName} numberOfLines={1}>
+                      {file.name}
+                    </Text>
+                  </View>
+                ) : (
+                  <View style={styles.placeholderContainer}>
+                    <Ionicons name="document-outline" size={40} color={COLORS.textSecondary} />
+                    <Text style={styles.placeholderText}>Tap to select a pdf file</Text>
+                  </View>
+                )}
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity 
+              style={styles.button} 
+              onPress={handleSubmit}
+              disabled={loading}
+            >
+              {loading ? (
+                <ActivityIndicator color={COLORS.white} />
+              ) : (
+                <>
+                  <Ionicons
+                    name="cloud-upload-outline"
+                    size={20}
+                    color={COLORS.white}
+                    style={styles.buttonIcon}
+                  />
+                  <Text style={styles.buttonText}>Update</Text>
+                </>
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
